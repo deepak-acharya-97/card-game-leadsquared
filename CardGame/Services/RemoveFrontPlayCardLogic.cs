a20585af2f26a8b0e59924eed8a2a31183cc931a
Deepak Acharya <aacharyadeepak100@gmail.com>
2020-12-13 19:47:07 +0530

    Update playcard logic

---
+++ CardGame/Services/RemoveFrontPlayCardLogic.cs
@@ -0,0 +1,19 @@
+﻿using CardGame.Interfaces;
+using CardGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CardGame.Services
+{
+    public class RemoveFrontPlayCardLogic : IPlayCardLogic
+    {
+        public CardBase PlayCard(List<CardBase> cards)
+        {
+            CardBase topOfDeck = cards.ElementAt(0);
+            cards.RemoveAt(0);
+            return topOfDeck;
+        }
+    }
+}
