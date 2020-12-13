befe4fe0d6b46be22224f4ea2985286910382426
Deepak Acharya <aacharyadeepak100@gmail.com>
2020-12-13 09:12:45 +0530

    Add Constant

---
+++ CardGame/Constant/CardConstant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame.Constant
+{
+    public class CardConstant
+    {
+        public static readonly int CardStart = 2;
+        public static readonly int CardNumericCount = 8;
+    }
+}
