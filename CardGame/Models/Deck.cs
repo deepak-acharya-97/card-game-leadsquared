d1fb58d8dea0e3f9ac6419432f99cc263926fc60
Deepak Acharya <aacharyadeepak100@gmail.com>
2020-12-13 07:51:52 +0530

    add deck model

---
+++ CardGame/Models/Deck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame.Models
+{
+    public class Deck
+    {
+        public IList<CardBase> Cards { get; private set; }
+
+        public Deck()
+        {
+            Cards = new List<CardBase>();
+        }
+    }
+}
