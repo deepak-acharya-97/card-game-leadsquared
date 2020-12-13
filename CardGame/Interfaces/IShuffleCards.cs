8343a81f10192f12a300fed5fda3ceea884ec5a7
Deepak Acharya <aacharyadeepak100@gmail.com>
2020-12-13 11:30:04 +0530

    Add ShuffleCards interface and implementation

---
+++ CardGame/Interfaces/IShuffleCards.cs
@@ -0,0 +1,12 @@
+﻿using CardGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame.Interfaces
+{
+    public interface IShuffleCards
+    {
+        void ShuffledCards(List<CardBase> cards);
+    }
+}
---
+++ CardGame/Services/ShuffleCards.cs
@@ -0,0 +1,17 @@
+﻿using CardGame.Interfaces;
+using CardGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CardGame.Services
+{
+    public class ShuffleCards : IShuffleCards
+    {
+        public void ShuffledCards(List<CardBase> cards)
+        {
+            cards = cards.OrderBy(card => Guid.NewGuid()).ToList();
+        }
+    }
+}
