95bf34f38bb01ac6c573c446dc95829d6d9b721b
Deepak Acharya <aacharyadeepak100@gmail.com>
2020-12-13 11:53:33 +0530

    interface for game and deck

---
+++ CardGame/Interfaces/IDeck.cs
@@ -0,0 +1,12 @@
+﻿using CardGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame.Interfaces
+{
+    public interface IDeck
+    {
+        List<CardBase> Cards { get; }
+    }
+}
---
+++ CardGame/Interfaces/IGame.cs
@@ -0,0 +1,14 @@
+﻿using CardGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame.Interfaces
+{
+    public interface IGame
+    {
+        CardBase PlayCard();
+        void ShuffleDeck();
+        void Restart();
+    }
+}
