5b380d9b6499eb4b43b6a0aa7368be3dbad74686
Deepak Acharya <aacharyadeepak100@gmail.com>
2020-12-13 11:54:05 +0530

    Implement game

---
+++ CardGame/Game.cs
@@ -0,0 +1,48 @@
+﻿using CardGame.Interfaces;
+using CardGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CardGame
+{
+    public class Game : IGame
+    {
+        private readonly IDeck _deck;
+        private List<CardBase> _cards;
+        private readonly IShuffleCards _shuffleCards;
+
+        public Game(IDeck deck, IShuffleCards shuffleCards)
+        {
+            _deck = deck;
+            _cards = deck.Cards;
+            _shuffleCards = shuffleCards;
+        }
+
+        public CardBase PlayCard()
+        {
+            CardBase topOfDeck = null;
+            try
+            {
+                topOfDeck = _cards.ElementAt(0);
+                _cards.RemoveAt(0);
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return topOfDeck;
+        }
+
+        public void Restart()
+        {
+            _cards = _deck.Cards;
+        }
+
+        public void ShuffleDeck()
+        {
+            _cards = _shuffleCards.GetShuffledCards(_cards);
+        }
+    }
+}
